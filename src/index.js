a9d52aab460376acc37b7c61379555f83133f4fc
ollie <oleg.laliev.developer@gmail.com>
2019-03-22 20:34:37 +0300

    feat: new completed task - advanced zero

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let getMainMultipliers = num => {
+
+     let isPrime = num => {
+       for (var i = 2; i < num; i++) if (num % i == 0) return false;
+       return num >= 2;
+     };
+
+     let multiplyObj = {};
+     for (let i = 2, end = num, helpNum = num; i <= end; i++) {
+       if (isPrime(i)){
+         while(helpNum % i === 0){
+           helpNum = helpNum / i;
+           (multiplyObj[i]) ? multiplyObj[i]++ : multiplyObj[i] = 1 ;
+         };
+       };
+     };
+     return multiplyObj;
+   };
+
+   let getMaxPrimePow = (number, prime) => {
+     let ourNumber = number, ourPrime = prime, powCounter = 0;
+     while( ourNumber > 0){
+       ourNumber = ~~(ourNumber/ourPrime);
+       powCounter += ourNumber;
+     };
+     return powCounter;
+   };
+
+   let baseMultipliers = getMainMultipliers(base);
+   let result = [];
+
+
+   for (i in baseMultipliers) {
+     let numberMultipliers = {};
+     numberMultipliers[i] = getMaxPrimePow(number,i);
+
+     result.push( numberMultipliers[i] / baseMultipliers[i] );
+   };
+
+   return ~~Math.min.apply(null, result);
 }
